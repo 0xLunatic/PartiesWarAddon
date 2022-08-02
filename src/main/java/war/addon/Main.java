8bdf38b60a74a17d6973a43690bda1104c5bb343
DreamerDotID <dreamerdotid@gmail.com>
2022-08-02 20:54:45 +0700

    Parties plugin detection

--- src/main/java/war/addon/Main.java
+++ src/main/java/war/addon/Main.java
@@ -1,15 +1,12 @@
 package war.addon;
 
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import war.addon.commands.PartiesWarCommand;
 import war.addon.data.DataManager;
 
 import java.io.File;
-import java.util.HashMap;
 import java.util.Objects;
-import java.util.UUID;
 
 public final class Main extends JavaPlugin {
 
@@ -30,10 +27,15 @@ public final class Main extends JavaPlugin {
             saveDefaultConfig();
         }
 
-        Objects.requireNonNull(getCommand("clanwars")).setExecutor(new PartiesWarCommand(this));
+        if (getServer().getPluginManager().isPluginEnabled("Parties")) {
+            Objects.requireNonNull(getCommand("clanwars")).setExecutor(new PartiesWarCommand(this));
 
-        getServer().getPluginManager().registerEvents(new PartiesWarCommand(this), this);
-        System.out.println("§aClan Wars Plugin Enabled!");
+            getServer().getPluginManager().registerEvents(new PartiesWarCommand(this), this);
+            System.out.println("§aClan Wars Plugin Enabled!");
+        }else{
+            System.out.println("§cParties Plugin not installed! Disabling the plugin!");
+            this.getPluginLoader().disablePlugin(this);
+        }
 
     }
 
